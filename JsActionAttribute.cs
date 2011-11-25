4504ecad96275f95bd31bded23daf4cf70a60c42
XVincentX_cp <SND\XVincentX_cp>
2011-11-25 09:08:41 +0000

    Documentation comment.

--- JsActionAttribute.cs
+++ JsActionAttribute.cs
@@ -3,6 +3,9 @@ using System.Web.Mvc;
 
 namespace JsAction
 {
+    /// <summary>
+    /// JsActionAttribute can be used to mark Action Method and generate related Javascript
+    /// </summary>
     [AttributeUsage(AttributeTargets.Method, Inherited = false, AllowMultiple = false)]
     public sealed class JsActionAttribute : Attribute
     {
@@ -10,7 +13,13 @@ namespace JsAction
         {
         }
 
+        /// <summary>
+        /// Gets or sets Javascript function name to generate. If null, Action name will be used.
+        /// </summary>
         public string MethodName { get; set; }
+        /// <summary>
+        /// Gets or sets Http Verb to use in Ajax request. Must be specified if multiple Verbs are accepted for same Action Method.
+        /// </summary>
         public HttpSingleVerb Verb { get; set; }
     }
 
