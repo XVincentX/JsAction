2f78fc981f784b69755b760745eca69603f220a7
XVincentX_cp <SND\XVincentX_cp>
2012-03-13 20:24:20 +0000

    0.5 prerelease

--- JsAction.MVC/JsActionHelperExtension.cs
+++ JsAction.MVC/JsActionHelperExtension.cs
@@ -1,7 +1,9 @@
 ﻿using System.Web;
 using System.Web.Mvc;
+using System;
+using System.ComponentModel;
 
-namespace System.Web.Mvc
+namespace JsAction
 {
     /// <summary>
     /// JsAction Helper extension
@@ -14,7 +16,27 @@ namespace System.Web.Mvc
         /// <param name="helper">static html helper</param>
         /// <param name="Groups">Groups name to import</param>
         /// <returns>Html String that represents script reference</returns>
+        [Obsolete("Consider to use @JsActionHelper.MVCScript, placed in System.Web.Mvc namespace", false)]
         public static MvcHtmlString JsScript(this HtmlHelper helper, params string[] Groups)
+        {
+            return MvcHtmlString.Create(string.Format("<script type=\"text/javascript\" src=\"{0}\"></script>", string.Format(VirtualPathUtility.ToAbsolute("~/JsAction?data={0}"), string.Join(",", Groups))));
+        }
+    }
+}
+
+namespace System.Web.Mvc
+{
+    /// <summary>
+    /// JsAction Helper
+    /// </summary>
+    public static class JsActionHelperExtension
+    {
+        /// <summary>
+        /// Writes into view the needed script reference for JsAction.
+        /// </summary>
+        /// <param name="Groups">Groups name to import</param>
+        /// <returns>Html String that represents script reference</returns>
+        public static MvcHtmlString JsActionScript(this HtmlHelper helper, params string[] Groups)
         {
             return MvcHtmlString.Create(string.Format("<script type=\"text/javascript\" src=\"{0}\"></script>", string.Format(VirtualPathUtility.ToAbsolute("~/JsAction?data={0}"), string.Join(",", Groups))));
         }
